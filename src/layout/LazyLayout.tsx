a0b9e784ac9f4e2eb3085dc372995041bdc7588f
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-16 17:10:00 -0300

    Nasted LazyLoad

---
+++ src/layout/LazyLayout.tsx
@@ -0,0 +1,12 @@
+import Navigation from "../01-lazyload/router/Navigation";
+
+export const LazyLayout = () => {
+  return (
+    <div>
+      <h1>Lazy Layout Page - MAIN</h1>
+      <Navigation/>
+    </div>
+  );
+};
+
+export default LazyLayout;
