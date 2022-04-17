dbb80163ed3cdc0b3764186068722b958611ca9a
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-16 22:18:02 -0300

    Component Compound finish

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,32 @@
+ import {ProductCard,  ProductButtons, ProductImg, ProductTitle } from '../components'; 
+
+ const product ={
+        id: '1', 
+        title: 'Coffe-Card',
+        img: './coffee-mug.png'
+  }
+const ShoppingPage = () => {
+  return (
+    <div>
+          <h1> Shopping Store  </h1>
+          <hr />
+        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+          
+              {/* OPCION A */}
+            <ProductCard product={product}>
+                      <ProductCard.Image/>
+                      <ProductCard.Title/>
+                      <ProductCard.Buttons />
+            </ProductCard>
+
+              {/* OPCION B */}
+              <ProductCard product={product}>
+                      <ProductImg/>
+                      <ProductTitle title='Cafe'/>
+                      <ProductButtons />
+            </ProductCard>
+        </div>
+    </div>
+  )
+}
+export default ShoppingPage
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom';
 
 import logo from '../logo.svg';
-
+import ShoppingPage from '../02-component-patterns/pages/ShoppingPage';
 export const Navigation = () => {
   return (
     <Router>
@@ -15,7 +15,7 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/shopping" activeClassName="nav-active" exact>Shopping</NavLink>
             </li>
             <li>
               <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
@@ -26,8 +26,6 @@ export const Navigation = () => {
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/about">
             <h1>About</h1>
@@ -36,7 +34,7 @@ export const Navigation = () => {
             <h1>Users</h1>
           </Route>
           <Route path="/">
-            <h1>Home</h1>
+            <ShoppingPage/>
           </Route>
         </Switch>
       </div>
