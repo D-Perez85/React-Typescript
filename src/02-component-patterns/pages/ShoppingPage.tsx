e7d2e2d303407daa35de2ee6b4d577ad68e5b6dc
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-30 13:42:20 -0300

    Reorder of Shopping Page

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,50 +1,47 @@
 import { ProductButtons, ProductImg, ProductTitle } from "../components";
 import { ProductCard } from "../components"; // Import para componente de estructura interna
 //import ProductCard from '../components/ProductCard';//Import para HOC
-
 import "../styles/custom-styles.css";
-
-
 import { products } from "../data/products"; //IMPORT DIRECTO SIN CUSTOM HOOK
-const product = products[0]; 
+
+const product = products[0];
+
 const ShoppingPage = () => {
   return (
     <div>
       <h1> Shopping Store </h1>
       <hr />
-        
-          <ProductCard
-            product={product}
-            className="bg-dark text-white"
-            key={product.id}
-            initialValues={{
-              count: 4,
-              maxCount: 10
-            }}>
+      <ProductCard
+        product={product}
+        className="bg-dark text-white"
+        key={product.id}
+        initialValues={{
+          count: 4,
+          maxCount: 10,
+        }}>
 
-              {
-                ({reset, count, increaseBy, isMaxCountReached})=> (
-                  <>
-                    <ProductImg className="custom-image" style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
-                    <ProductTitle title={product.title} className="text-white text-bold"/>
-                    <ProductButtons className="custom-buttons" style={{ display: "flex", justifyContent: "center" }}/>
-                  
-                    {/* {JSON.stringify(args, null, 3)} */}
-                    <button onClick={reset}> RESET</button>
-                    <button onClick={() => increaseBy(-2)}> - 2</button> 
+        {({ reset, count, increaseBy, isMaxCountReached }) => (
+          <>
+            <ProductImg
+              className="custom-image"
+              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
+            <ProductTitle
+              title={product.title}
+              className="text-white text-bold"/>
+            <ProductButtons
+              className="custom-buttons"
+              style={{ display: "flex", justifyContent: "center" }}/>
 
-                    {
-                    (!isMaxCountReached && <button onClick={() => increaseBy(2)}> + 2</button>)
-                    }
-
-                    <span>{count}</span>
-                  </>
-
-                )
-              }
-          </ProductCard>
-       
- 
+            {/* {JSON.stringify(args, null, 3)} */}
+            <button onClick={reset}> RESET</button>
+            <button onClick={() => increaseBy(-2)}> - 2</button>
+              {!isMaxCountReached && (
+                <button onClick={() => increaseBy(2)}> + 2</button>
+              )}
+            <span>{count}</span>
+          </>
+        )}
+      </ProductCard>
     </div>
   );
 };
