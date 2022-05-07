74d8f428bc1094316be954966b66bbff1b803079
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-07 13:16:47 -0300

    Dynamic Form - Dynamic Select Component

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -4,10 +4,10 @@ import formJson from "../data/custon-form.json";
 import * as Yup from "yup";
 
 const initialValues: { [key: string]: any } = {};
-    
-    for (const input of formJson) {
-        initialValues[input.name] = input.value;
-        }
+
+for (const input of formJson) {
+  initialValues[input.name] = input.value;
+}
 
 export const DynamicForm = () => {
   return (
@@ -21,16 +21,29 @@ export const DynamicForm = () => {
       >
         {(formik) => (
           <Form>
-            {formJson.map(({ type, name, placeholder, label }) => {
-              return (
-                <MyTextInput
-                  key={name}
-                  name={name}
-                  type={type as any}
-                  label={label}
-                  placeholder={placeholder}
-                />
-              );
+            {formJson.map(({ type, name, placeholder, label, options }) => {
+              if (type === "input" || type === "password" || type === "email") {
+                return (
+                  <MyTextInput
+                    key={name}
+                    name={name}
+                    type={type as any}
+                    label={label}
+                    placeholder={placeholder}/>
+                );
+              } else if (type === "select") {
+                return (
+                  <MySelect key={name} label={label} name={name}>
+                    <option value="">Select an option</option>
+                    {options?.map(({ id, label }) => (
+                      <option key={id} value={id}>
+                        {label}
+                      </option>
+                    ))}
+                  </MySelect>
+                );
+              }
+              throw new Error(`El type: ${type}, no es soportado`);
             })}
             <button type="submit"> SUBMIT </button>
           </Form>
