c3fbc61c0a7c4c050667ec931e909e5348eaa406
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-07 12:52:03 -0300

    Dynamic Form - InitialValues

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,51 +1,41 @@
-import { Formik, Form } from 'formik';
-import { MySelect, MyTextInput } from '../components';
-import formJson from '../data/custon-form.json';
-import * as Yup from 'yup';
-import React from 'react'
+import { Formik, Form } from "formik";
+import { MySelect, MyTextInput } from "../components";
+import formJson from "../data/custon-form.json";
+import * as Yup from "yup";
+
+const initialValues: { [key: string]: any } = {};
+    
+    for (const input of formJson) {
+        initialValues[input.name] = input.value;
+        }
 
 export const DynamicForm = () => {
   return (
     <div>
-        <h1>
-
-        DynamicForm
-        </h1>
-        <Formik initialValues={{
-            firstName:  '', 
-            lastName: ''
+      <h1>DynamicForm</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          console.log(values);
         }}
-        onSubmit={(values)=>{
-            console.log(values);
-            
-        }}
-        >
-            {(formik)=>(
-                <Form>
-                    { formJson.map( ({ type, name, placeholder, label }) => {
-                        return (
-
-                            <MyTextInput 
-                            key={name}
-                            name={name} 
-                            type={(type as any)} 
-                            label={label} 
-                            placeholder={placeholder}
-                            
-                            />
-                        )
-                    })}
-
-
-                         <button type='submit'> SUBMIT </button>
-                </Form>
-            )}
-        </Formik>
-        
-        </div>
-
-  )
-}
-
-
-
+      >
+        {(formik) => (
+          <Form>
+            {formJson.map(({ type, name, placeholder, label }) => {
+              return (
+                <MyTextInput
+                  key={name}
+                  name={name}
+                  type={type as any}
+                  label={label}
+                  placeholder={placeholder}
+                />
+              );
+            })}
+            <button type="submit"> SUBMIT </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
