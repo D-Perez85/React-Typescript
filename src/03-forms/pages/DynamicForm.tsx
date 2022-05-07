f5026f8f83c0f96a46baf340ce6b62ab40b19205
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-07 16:27:38 -0300

    Dynamic Form - Dynamic Validations

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,35 +1,72 @@
+import formJson from "../data/custon-form.json";
 import { Formik, Form } from "formik";
 import { MySelect, MyTextInput } from "../components";
-import formJson from "../data/custon-form.json";
 import * as Yup from "yup";
 
 const initialValues: { [key: string]: any } = {};
+const requiredFields: { [key: string]: any } = {};
 
 for (const input of formJson) {
   initialValues[input.name] = input.value;
+
+  if (!input.validations) continue;
+
+  let schema = Yup.string();
+
+  for (const rule of input.validations) {
+    if (rule.type === "required") {
+      schema = schema.required("Este campo es requerido");
+    }
+
+    //SI EL OBJETO NO TRAE EL VALOR DE LOS CARACTERES SE ASIGNA EL VALOR ACA
+    if (rule.type === "minLength") {
+      schema = schema.min(
+        (rule as any).value || 2,
+        `Mínimo de ${(rule as any).value || 2} caracteres`
+      );
+    }
+
+    if (rule.type === "maxLength") {
+      schema = schema.max(
+        (rule as any).value || 12,
+        `Maximo de ${(rule as any).value || 12} caracteres`
+      );
+    }
+
+    if (rule.type === "email") {
+      schema = schema.email(`Revise el formato del email`);
+    }
+  }
+
+  requiredFields[input.name] = schema;
 }
+
+const validationSchema = Yup.object({ ...requiredFields });
 
 export const DynamicForm = () => {
   return (
     <div>
-      <h1>DynamicForm</h1>
+      <h1>Dynamic Form</h1>
+
       <Formik
         initialValues={initialValues}
+        validationSchema={validationSchema}
         onSubmit={(values) => {
           console.log(values);
         }}
       >
         {(formik) => (
-          <Form>
+          <Form noValidate>
             {formJson.map(({ type, name, placeholder, label, options }) => {
               if (type === "input" || type === "password" || type === "email") {
                 return (
                   <MyTextInput
                     key={name}
-                    name={name}
                     type={type as any}
+                    name={name}
                     label={label}
-                    placeholder={placeholder}/>
+                    placeholder={placeholder}
+                  />
                 );
               } else if (type === "select") {
                 return (
@@ -43,9 +80,11 @@ export const DynamicForm = () => {
                   </MySelect>
                 );
               }
+
               throw new Error(`El type: ${type}, no es soportado`);
             })}
-            <button type="submit"> SUBMIT </button>
+
+            <button type="submit">Submit</button>
           </Form>
         )}
       </Formik>
